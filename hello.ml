fa6ea40c716a92b478ea9c991b433d291123cb4b
Mauricio Fernandez <mfp@acm.org>
2009-01-01 11:39:44 +0100

    Added "hello", minimal eliom module to serve as baseline for bms.

---
+++ hello.ml
@@ -0,0 +1,16 @@
+open Eliom_parameters
+open Lwt
+
+let service = lazy begin
+  Eliom_predefmod.Text.register_new_service
+    ~path:[""]
+    ~get_params:unit
+    (fun sp () () -> return ("Hello, world\n", "text/plain"))
+end
+
+let () =
+  Eliom_services.register_eliom_module "hello"
+    (fun () -> ignore (Lazy.force service))
+
+let () = Ocsigen_server.start_server ()
+
