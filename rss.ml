55abf1ed90930d8e2f89331e11aa386ec8cea269
Mauricio Fernandez <mfp@acm.org>
2009-01-02 12:16:29 +0100

    Added Rss.make_rdf.

--- rss.ml
+++ rss.ml
@@ -8,9 +8,9 @@ type image = {
 }
 
 type item = {
-  it_title : string option;
-  it_link : string option;
-  it_description : string option;
+  it_title : string;
+  it_link : string;
+  it_description : string;
   it_author : string option;
   it_category : string option;
   it_comments : string option;
@@ -20,21 +20,27 @@ type item = {
   (* it_source:  *)
 }
 
+let item_date it = it.it_pubDate
+
 let elm kind ?(attrs = []) v = Node(kind, attrs, [PCDATA v])
 let opt_elm kind ?attrs = Option.map (elm kind ?attrs)
 let some_elm kind ?attrs v = Some (elm kind ?attrs v)
 
 let format_date t = Netdate.mk_mail_date t
+let format_date_rdf t = Netdate.format "%Y-%m:%dT%H:%M:%S%z" (Netdate.create t)
 
 let filter_concat ls = List.filter_map (fun x -> x) (List.concat ls)
+let filter_some = List.filter_map (fun x -> x)
+
+let encode_pcdata s = [PCDATA s] (* TODO: use CDATA instead? *)
 
 let xml_of_item item =
   Node
     ("item", [],
      filter_concat
-       [[opt_elm "title" item.it_title;
-         opt_elm "link" item.it_link;
-         opt_elm "description" item.it_description;
+       [[some_elm "title" item.it_title;
+         some_elm "link" item.it_link;
+         some_elm "description" item.it_description;
          opt_elm "author" item.it_author;
          opt_elm "category" item.it_category;
          opt_elm "comments" item.it_comments;
@@ -80,8 +86,58 @@ let make
     Node("rss", [string_attrib "version" "2.0"],
          [Node("channel", [], channel)])
 
+let make_rdf
+      ~title ~link ~description ~date ?language ?copyright ?image items =
+  let it_list_elm =
+    Node
+      ("items", [],
+       [Node ("rdf:Seq", [],
+             List.map
+               (fun it -> Leaf ("rdf:li", [string_attrib "resource" it.it_link]))
+               items)]) in
+  let channel_elm =
+    Node("channel", [string_attrib "rdf:about" link],
+         filter_some
+           [some_elm "title" title;
+            some_elm "link" link;
+            some_elm "description" description;
+            Option.map
+              (fun img -> Leaf("image", [string_attrib "rdf:resource" img.img_url]))
+              image;
+            opt_elm "dc:language" language;
+            opt_elm "dc:rights" copyright;
+            some_elm "dc:date" (format_date_rdf date);
+            Some it_list_elm]) in
+  let image_elm =
+    Option.map
+      (fun img ->
+         [Node ("image", [string_attrib "rdf:about" img.img_url],
+               [elm "title" img.img_title;
+                elm "link" img.img_link;
+                elm "url" img.img_url])])
+      image in
+  let elm_of_item it =
+    let c = Node("content:encoded", [], encode_pcdata it.it_description) in
+      Node ("item", [string_attrib "rdf:about" it.it_link],
+            (elm "title" it.it_title ::
+             elm "link" it.it_link ::
+             c ::
+             Option.map_default
+               (fun date -> [elm "dc:date" (format_date_rdf date)])
+               []
+               it.it_pubDate));
+  in Node
+       ("rdf:RDF",
+        [string_attrib "xmlns" "http://purl.org/rss/1.0/";
+         string_attrib "xmlns:dc" "http://purl.org/dc/elements/1.1/";
+         string_attrib "xmlns:rdf" "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+         string_attrib "xmlns:content" "http://purl.org/rss/1.0/modules/content/";
+         string_attrib "xml:lang" "en"],
+        List.concat
+          [[channel_elm]; Option.default [] image_elm; List.map elm_of_item items])
+
 let make_item
-      ?title ?link ?description ?author ?category ?comments ?pubDate ?guid () =
+      ~title ~link ~description ?author ?category ?comments ?pubDate ?guid () =
   {
     it_title = title;
     it_link = link;
--- rss.mli
+++ rss.mli
@@ -1,15 +1,7 @@
 type image = { img_url : string; img_title : string; img_link : string; }
+type item
 
-type item = {
-  it_title : string option;
-  it_link : string option;
-  it_description : string option;
-  it_author : string option;
-  it_category : string option;
-  it_comments : string option;
-  it_pubDate : float option;
-  it_guid : (string * bool) option;
-}
+val item_date : item -> float option
 
 val make :
   title:string -> link:string -> description:string ->
@@ -18,6 +10,10 @@ val make :
   ?category:string -> ?generator:string -> ?ttl:int -> ?image:image -> 
   item list -> XML.elt
 
-val make_item : ?title:string -> ?link:string -> ?description:string ->
+val make_item : title:string -> link:string -> description:string ->
   ?author:string -> ?category:string -> ?comments:string -> ?pubDate:float -> 
   ?guid:string * bool -> unit -> item
+
+val make_rdf : title:string -> link:string -> description:string ->
+  date:float -> ?language:string -> ?copyright:string -> ?image:image ->
+  item list -> XML.elt
